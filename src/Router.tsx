2747c3f49187da0039242ee3e6c106458f03ebaa
Felipe <felipecss88@gmail.com>
2023-02-17 08:01:50 -0300

    testando

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import { Success } from './pages/Success'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<Checkout />}></Route>
       <Route path="/Checkout" element={<Checkout />}></Route>
       <Route path="/Success" element={<Success />}></Route>
     </Routes>
