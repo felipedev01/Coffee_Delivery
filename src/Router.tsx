194ea429234abc762f288f7f3544eea7d738f22d
Felipe <felipecss88@gmail.com>
2023-02-17 08:21:56 -0300

    testando rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import { Success } from './pages/Success'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Checkout />}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/Checkout" element={<Checkout />}></Route>
       <Route path="/Success" element={<Success />}></Route>
     </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <nav>
+      <NavLink to="/" title="Timer">
+        <Timer size={24} />
+      </NavLink>
+
+      <NavLink to="/Checkout">
+        <Scroll size={24} />
+      </NavLink>
+    </nav>
+  )
 }
