34f8149ee9035f6e807128525f97cdc9c19ed093
Felipe <felipecss88@gmail.com>
2023-02-17 06:57:03 -0300

    eslint aplicado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-
-import {ThemeProvider} from 'styled-components'
-import { DefaultTheme} from './styles/Themes/default'
+import { ThemeProvider } from 'styled-components'
+import { DefaultTheme } from './styles/Themes/default'
 import { GlobalStyled } from './styles/global'
 
-
 function App() {
-
-
   return (
     <ThemeProvider theme={DefaultTheme}>
-       <h1>Ola Mundo!</h1>
-       <GlobalStyled />
-      
+      <h1>Ola Mundo!</h1>
+      <GlobalStyled />
     </ThemeProvider>
-   
   )
 }
 
