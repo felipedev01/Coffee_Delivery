dbc8bcf436d91174ed422fe54b939e8b5da9494e
Felipe <felipecss88@gmail.com>
2023-02-18 18:50:52 -0300

    Definindo layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/defaultLayout'
 import { Checkout } from './pages/Checkout'
 import { Home } from './pages/Home'
 import { Success } from './pages/Success'
@@ -6,9 +7,11 @@ import { Success } from './pages/Success'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/Checkout" element={<Checkout />}></Route>
-      <Route path="/Success" element={<Success />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Checkout" element={<Checkout />}></Route>
+        <Route path="/Success" element={<Success />}></Route>
+      </Route>
     </Routes>
   )
 }
